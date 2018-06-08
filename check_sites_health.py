09c668e30891583da36820caddeaa16927e389f6
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-06-08 22:56:12 +0300

    fix dirty function

--- check_sites_health.py
+++ check_sites_health.py
@@ -11,24 +11,25 @@ def load_urls4check(path):
         return url_list
 
 
-def is_server_respond_ok(url):
-    try:
-        response_from_url = requests.get(url)
-        return response_from_url.ok
-    except requests.ConnectionError:
-        return None
+def is_server_respond_ok(url_list):
+    for url in url_list:
+        try:
+            response_from_url = requests.get(url)
+            yield response_from_url.ok
+        except requests.ConnectionError:
+            return None
 
 
-def is_domain_paid(url_list, paid_days):
+def is_domains_paid(url_list, paid_days, server_respond_ok):
     today = datetime.datetime.today()
     for url in url_list:
         expiration_date = get_domain_expiration_date(url)
         if expiration_date is None:
             return None
         if expiration_date - today >= datetime.timedelta(paid_days):
-            yield url, True
+            yield url, True, server_respond_ok
         else:
-            return None, None
+            return None, None, None
 
 
 def get_domain_expiration_date(url):
@@ -41,15 +42,12 @@ def get_domain_expiration_date(url):
 
 
 def print_site_health(url_and_site_paid):
-    for url, site_paid in url_and_site_paid:
+    for url, site_paid, server_respond in url_and_site_paid:
         is_paid = "Да" if site_paid else "Нет"
-        server_respond = is_server_respond_ok(url)
         is_respond_ok = "Да" if server_respond else "Нет"
-        print(
-            "Сайт: ", url,
-            "Код состояния сервера 200: ", is_respond_ok,
-            "Проплачено на месяц вперед: ", is_paid
-        )
+        print("Сайт: ", url)
+        print("Код состояния сервера 200: ", is_respond_ok)
+        print("Проплачено на месяц вперед: ", is_paid)
 
 
 if __name__ == "__main__":
@@ -61,5 +59,6 @@ if __name__ == "__main__":
         exit("Файла нет в директории")
     paid_days = 30
     url_list = load_urls4check(filepath)
-    url_and_site_paid = is_domain_paid(url_list, paid_days)
+    server_respond_ok = is_server_respond_ok(url_list)
+    url_and_site_paid = is_domains_paid(url_list, paid_days, server_respond_ok)
     print_site_health(url_and_site_paid)
